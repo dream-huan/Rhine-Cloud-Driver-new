1665328346ed80798ae44ef26e5921fe3dd2b604
dream-huan <52626649+dream-huan@users.noreply.github.com>
2022-12-08 10:51:36 +0000

    //

--- common/fileoperator.go
+++ common/fileoperator.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Mkdir(dirName string) bool {
-	err := os.Mkdir("/rhine-cloud-driver/uploads/"+dirName, 0777)
+	err := os.Mkdir("rhine-cloud-driver/uploads/"+dirName, 0777)
 	if err != nil {
 		log.Logger.Error("新建文件夹错误", zap.Error(err))
 		return false
